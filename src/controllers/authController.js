30b383c95984ce0e3edac2b79f106f0d3dfd80b6
MariaDuda <mariabevenuto062@gmail.com>
2022-07-06 11:03:20 -0300

    creat authrouts && controllers && shcemas

---
+++ src/controllers/authController.js
@@ -0,0 +1,15 @@
+
+import joi from "joi";
+import bcypt from "bcrypt"
+import db from "../db/mongo.js"
+import authRegisterSchema from "../schemas/authSchema.js"
+
+
+export default function creatUser(req,res){
+try{
+  
+
+}catch(e){
+    console.log(e)
+}
+}
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+
+const  router = Router()
+ 
+
+router.post("sing-up" , creatUser)
+
+export default router;
