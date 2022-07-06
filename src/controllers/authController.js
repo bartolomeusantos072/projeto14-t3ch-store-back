58fed1e2c6257ba64c7716f5c58a175e6591e2b2
MariaDuda <mariabevenuto062@gmail.com>
2022-07-06 12:08:41 -0300

    bcrypt mix password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 
 import db from "../db/mongo.js"
+import bcrypt from "bcrypt"
 import authRegisterSchema from "../schemas/authSchema.js"
 
 
@@ -15,8 +16,13 @@ export async function creatUser(req,res){
         return res.status(422).send("email ou senha estão incorretos")
     }
 try{
+    const verifyEmail = await db.collection("users").findOne({email})
+    if(verifyEmail){
+        return res.status(400).send("email inválido")
+    }
+    const misticPassword = bcrypt.hashSync(password , 10)
 
-   await db.collection("users").insertOne({username , email , password , confirmPassword})
+   await db.collection("users").insertOne({username , email , password:misticPassword })
 
    return res.send("foi")
 
